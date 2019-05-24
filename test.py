bcb4a3933eac1352f5ba03ae98080a7426dd1952
Klement Omeri <klement-omeri97@hotmail.com>
2019-05-24 17:02:44 +0200

    Create test class for single_equation and testing all of the functions inside it.

---
+++ test.py
@@ -0,0 +1,33 @@
+import unittest
+import math
+import single_equations as se
+
+
+class SingleEquationsTestCase(unittest.TestCase):
+
+    def test_bisection(self):
+        self.assertAlmostEqual(se.bisection(f=lambda x: x + math.exp(x) - 2,
+                                            a=0, b=1),
+                               0.442855, 5)
+
+    def test_fixed_point(self):
+        self.assertAlmostEqual(se.fixed_point(f=lambda x: math.pi + math.atan(x),
+                                              x0=math.pi),
+                               4.493410, 5)
+
+    def test_lagrange(self):
+        self.assertAlmostEqual(se.lagrange(f=lambda x: x + math.exp(x) - 2,
+                                           a=0, b=1),
+                               0.442855, 5)
+
+    def test_newton(self):
+        self.assertAlmostEqual(se.newton(f=lambda x: x + math.exp(x) - 2,
+                                         df=lambda x: 1+math.exp(x),
+                                         x0=0),
+                               0.442855, 5)
+
+    def test_combined(self):
+        self.assertAlmostEqual(se.combined(f=lambda x: x + math.exp(x) - 2,
+                                           df=lambda x: 1+math.exp(x),
+                                           a=0, b=1),
+                               0.442855, 5)
